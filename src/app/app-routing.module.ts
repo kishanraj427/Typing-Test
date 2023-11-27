2399c5f829b58a3220a6c8849fe38218b8c7aef6
kishanraj427 <62000613+kishanraj427@users.noreply.github.com>
2023-11-28 00:01:12 +0530

    Typing Test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SimpleTestComponent } from './simple-test/simple-test.component';
+import { AdvancedTestComponent } from './advanced-test/advanced-test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "/simple", pathMatch: 'full'},
+  {path: "simple", component: SimpleTestComponent},
+  {path: "advanced", component: AdvancedTestComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { SimpleTestComponent } from './simple-test/simple-test.component';
+import { AdvancedTestComponent } from './advanced-test/advanced-test.component';
+import { TestBoxComponent } from './test-box/test-box.component';
+import { TestDurationComponent } from './test-duration/test-duration.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TimePipe } from './pipe/time.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidenavComponent,
+    SimpleTestComponent,
+    AdvancedTestComponent,
+    TestBoxComponent,
+    TestDurationComponent,
+    TimePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
